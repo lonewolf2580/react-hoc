dfda771ccaad60159a10869c4b635abb505c4ddb
lonewolf2580 <linusngwee@yahoo.com>
2021-10-17 07:32:38 -0700

    Post - 2 files modified

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,23 +1,36 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class Post extends Component {
   state = {
-    id: null,
+    post: null,
   };
   componentDidMount() {
     // console.log(this.props.match.params.post_id);
     let id = this.props.match.params.post_id;
-    this.setState({
-      id: id,
-    });
+    axios
+      .get('https://jsonplaceholder.typicode.com/posts/' + id)
+      .then((res) => {
+        this.setState({
+          post: res.data,
+        });
+        // console.log(this.state.post);
+      });
+    // this.setState({
+    //   id: id,
+    // });
     // console.log(id);
   }
   render() {
-    return (
-      <div className="container">
-        <h4>{this.state.id}</h4>
+    const post = this.state.post ? (
+      <div className="post">
+        <h4 className="center">{this.state.post.title}</h4>
+        <p>{this.state.post.body}</p>
       </div>
+    ) : (
+      <div className="center">Loading Post...</div>
     );
+    return <div className="container">{post}</div>;
   }
 }
 
