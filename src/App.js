7c0dda27d0aaaca0cbe7237abeaa633010cbe95a
lonewolf2580 <linusngwee@yahoo.com>
2021-10-17 07:14:51 -0700

    Route Params - 1 file modified, 1 added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar';
 import Home from './components/Home';
 import About from './components/About';
 import Contact from './components/Contact';
+import Post from './components/Post';
 import { BrowserRouter, Route } from 'react-router-dom';
 
 export default function App() {
@@ -13,6 +14,7 @@ export default function App() {
         <Route exact path="/" component={Home} />
         <Route path="/about" component={About} />
         <Route path="/contact" component={Contact} />
+        <Route path="/:post_id" component={Post} />
       </div>
     </BrowserRouter>
   );
