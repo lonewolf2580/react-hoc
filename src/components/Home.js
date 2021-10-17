fa997215952bdc16a56a3aa41b73225f08268770
lonewolf2580 <linusngwee@yahoo.com>
2021-10-17 06:04:20 -0700

    3 files modified

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,17 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 
-const Home = () => {
-  return (
-    <div className="container">
-      <h4 className="center">Home</h4>
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cumque hic
-        voluptas, aliquam a deserunt unde eligendi iure recusandae, quibusdam
-        maiores atque optio. Sapiente fuga placeat in, inventore ex recusandae
-        saepe?
-      </p>
-    </div>
-  );
-};
+class Home extends Component {
+  componentDidMount() {
+    axios.get('https://jsonplaceholder.typicode.com/posts').then((res) => {
+      console.log(res);
+    });
+  }
+  render() {
+    return (
+      <div className="container">
+        <h4 className="center">Home</h4>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cumque hic
+          voluptas, aliquam a deserunt unde eligendi iure recusandae, quibusdam
+          maiores atque optio. Sapiente fuga placeat in, inventore ex recusandae
+          saepe?
+        </p>
+      </div>
+    );
+  }
+}
 
 export default Home;
