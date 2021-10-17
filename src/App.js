6dc4a5f55c0e5fe39e599e2bf09aac79c251dccb
lonewolf2580 <linusngwee@yahoo.com>
2021-10-17 07:38:19 -0700

    Switch - 1 file modified

--- src/App.js
+++ src/App.js
@@ -4,17 +4,19 @@ import Home from './components/Home';
 import About from './components/About';
 import Contact from './components/Contact';
 import Post from './components/Post';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 export default function App() {
   return (
     <BrowserRouter>
       <div className="App">
         <Navbar />
-        <Route exact path="/" component={Home} />
-        <Route path="/about" component={About} />
-        <Route path="/contact" component={Contact} />
-        <Route path="/:post_id" component={Post} />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/contact" component={Contact} />
+          <Route path="/:post_id" component={Post} />
+        </Switch>
       </div>
     </BrowserRouter>
   );
